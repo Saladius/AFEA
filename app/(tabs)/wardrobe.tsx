b3085415bce339866946223dada5b472494a292a
ayouubmzariiii <ayoubmzari0@gmail.com>
2025-06-26 00:55:37 +0100

    Updated wardrobe.tsx

--- app/(tabs)/wardrobe.tsx
+++ app/(tabs)/wardrobe.tsx
@@ -14,7 +14,7 @@ import {
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRouter } from 'expo-router';
-import { Search, Filter, MoreVertical, Plus, X, Edit3, Share, Trash2 } from 'lucide-react-native';
+import { Search, Filter, MoveVertical as MoreVertical, Plus, X, CreditCard as Edit3, Share, Trash2 } from 'lucide-react-native';
 import { useClothes } from '@/hooks/useClothes';
 import { useAuth } from '@/hooks/useAuth';
 import { ClothingItem } from '@/types/database';
